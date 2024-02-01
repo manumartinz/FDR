e28ea085f1be648a5a44b42738e4818167e7ba74
Manuel A. Martínez <67569429+manumartinz@users.noreply.github.com>
2024-02-01 14:28:51 -0300

    FDR - JS
    Se avanzó tanto en FDR como en la sección del JS

---
+++ Javascript/intro.js
@@ -0,0 +1,17 @@
+/* 
+
+    Tipo de variables
+    - var
+    - let
+    - const
+
+    Tipo de datos primitivos
+    - string
+    - number
+    - bigint
+    - null
+    - undefined
+    - boolean 
+    - symbol?
+
+*/
